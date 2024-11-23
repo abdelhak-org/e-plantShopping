099127fb1fab7a3f4bacecdb35edd2fdc040fb83
abdelhak-org <abdelhak.org@gmail.com>
2024-11-23 08:22:39 +0100

    the modified code

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
@@ -9,27 +9,47 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+  const total = cart.reduce((accutotal , item)=>{
+   const itemCost = parseFloat(item.cost.slice(1));
+   return accutotal + itemCost*item.quantity
+  },0)
+
+   return total 
+  
   };
 
   const handleContinueShopping = (e) => {
+    alert('Functionality to be added for future reference');
+
+  };
+
+
+////// 
+  const handleIncrement = (item) => {
+   let {name , quantity} = item;
+   quantity += 1 
+    dispatch(updateQuantity({name , quantity}))
+    
+
    
   };
-
-
-
-  const handleIncrement = (item) => {
-  };
-
+///////////////
   const handleDecrement = (item) => {
+    let {name , quantity} = item;
+    if (name && quantity > 1){
+      quantity -= 1
+    }
+     dispatch(updateQuantity({name , quantity}))
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.slice(1) )* item.quantity
   };
 
   return (
